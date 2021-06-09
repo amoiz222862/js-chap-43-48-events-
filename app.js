6608932ae98929cb116952c5b288b52fb9b68a6d
49985 <amoiz222862@gmail.com>
2021-06-09 22:56:34 +0500

    except task 5

---
+++ app.js
@@ -0,0 +1,76 @@
+// ********************************* Chap 43-48 ****************************************
+
+
+
+//  TASK 1
+
+// function ale() {
+//         alert("hy guys");
+// }
+
+
+
+
+// TASK 2 
+
+// function f() {
+//         alert("THIS MOBILE IS VERY OXM");
+// }
+// function s() {
+//         alert("THIS MOBILE IS VERY nice");
+// }
+// function t() {
+//         alert("THIS MOBILE IS VERY pappu");
+// }
+
+
+
+//  TASK  3
+
+
+// function del() {
+//         var tr1 = document.getElementById("tr1");
+//         tr1.innerHTML = "";
+// }
+// function del1() {
+//         var tr2 = document.getElementById("tr2");
+//         tr2.innerHTML = "";
+// }
+// function del2() {
+//         var tr3 = document.getElementById("tr3");
+//         tr3.innerHTML = "";
+// }
+// function del3() {
+//         var tr4 = document.getElementById("tr4");
+//         tr4.innerHTML = "";
+// }
+// function del4() {
+//         var tr5 = document.getElementById("tr5");
+//         tr5.innerHTML = "";
+// }
+// function del5() {
+//         var tr6 = document.getElementById("tr6");
+//         tr6.innerHTML = "";
+// }
+// function del6() {
+//         var tr7 = document.getElementById("tr7");
+//         tr7.innerHTML = "";
+// }
+// function del7() {
+//         var tr8 = document.getElementById("tr8");
+//         tr8.innerHTML = "";
+// }
+// function del8() {
+//         var tr9 = document.getElementById("tr9");
+//         tr9.innerHTML = "";
+// }
+// function del9() {
+//         var tr10 = document.getElementById("tr10");
+//         tr10.innerHTML = "";
+// }
+
+
+
+// TASK 5
+
+
